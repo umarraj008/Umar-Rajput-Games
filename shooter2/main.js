2c4eb99c49cb468977c7159556e1ce6432b59031
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2019-10-12 23:38:11 +0100

    Add files via upload

--- shooter2/main.js
+++ shooter2/main.js
@@ -4,14 +4,20 @@ ctx.scale(1,1);
 let lastTime = 0;
 var dt;
 
+
+//testing
+var s1 = true;
+var s2 = true;
+
+
 var scrollOffset = {
     x: 0,
     y: 0,
 }
 
 var guns = {
-    pistol: {r: 20000, s: 10, i: 20},
-    ak: {r: 20000, s: 10, i: 20},
+    pistol: {r: 20000, s: 20, i: 500, d: 25},
+    ak: {r: 20000, s: 20, i: 50, d: 100},
 }
 
 var player = {
@@ -32,10 +38,13 @@ var player = {
     shoot: false,
     canShoot: true,
     health: 100,
+    canDamage: true,
+    gracePeriodCount: 0,
 }
 
 var playerIMG = document.getElementById("player");
 var playerShootIMG = document.getElementById("shoot");
+var playerDamageImg = document.getElementById("damage");
 
 var enemies = [];
 var gridRows = [];
@@ -43,18 +52,36 @@ var gridCols = [];
 
 var score = 0;
 var gameOver = false;
+var barColor = "lime";
+
+var EnemyParticles = [];
+var particleSpots = [];
+
+class particle {
+    constructor(xPOS, yPOS, size, col, dec) {
+        this.x = xPOS;
+        this.y = yPOS;
+        this.s = size;
+        this.color = col;
+        this.decay = dec;
+        this.count = 0;
+    }
+}
 
 class Enemy {
-    constructor(xPOS, yPOS, size, range) {
+    constructor(xPOS, yPOS, size, range, health, C, D) {
         this.x = xPOS;
         this.y = yPOS;
         this.s = size;
         this.r = range;
+        this.h = health;
+        this.color = C;
+        this.damage = D;
     }
 }
 
 class bull {
-    constructor(X, Y, R, S) {
+    constructor(X, Y, R, S, D) {
         this.speed = S;
         this.x = X;
         this.y = Y;
@@ -62,6 +89,7 @@ class bull {
         this.yv = this.speed * Math.sin(player.a - (90 / 180 * Math.PI));
         this.range = R;
         this.count = 0;
+        this.damage = D;
     }
 }
 
@@ -72,8 +100,8 @@ var ShootCount = 0;
 //var ShootInterval = 1000;
 
 //ENEMIES SETUP
-for (i = 0; i < 200; i++) {
-    SpawnEnemy(300);    
+for (i = 0; i < 1000; i++) {
+    SpawnEnemy(300, 100, "red", 10);    
 }
 
 var ROWoffset = 0;
@@ -94,7 +122,7 @@ for (i = 0; i < cols; i++) {
     COLSoffset += c.height / cols;
 }
 
-function SpawnEnemy(start) {
+function SpawnEnemy(start, health, col, dam) {
     var xP = getRandomInt(-2000, 2000);
     var yP = getRandomInt(-2000, 2000);
     
@@ -103,10 +131,14 @@ function SpawnEnemy(start) {
         yP = getRandomInt(-2000, 2000);
         
         if((Math.abs(xP) > start && Math.abs(yP) > start)) {
-            enemies.push(new Enemy(xP, yP, 30, 500));
+            enemies.push(new Enemy(xP, yP, 30, 500, health, col, dam));
             return;
         }
     }
+}
+
+function SpawnEnemy2(x, y, health, col, dam, siz) {
+    enemies.push(new Enemy(x, y, siz, 500, health, col, dam));
 }
 
 function draw() {
@@ -142,6 +174,7 @@ function draw() {
     //player
     //ctx.fillStyle = "blue";
     //ctx.fillRect(0 - 25, 0 - 25, 50, 50);
+
     ctx.drawImage(player.image, -100, -100, 200, 200);
     
     ctx.restore();
@@ -151,16 +184,39 @@ function draw() {
     //set world pos
     ctx.translate(960, 540);
     
-    
+    for (i = 0; i < particleSpots.length; i++) {
+        for(p = 0; p < particleSpots[i].length; p++) {
+            ctx.fillStyle = particleSpots[i][p].color;    
+            ctx.fillRect(particleSpots[i][p].x - (particleSpots[i][p].s / 2) + scrollOffset.x, particleSpots[i][p].y - (particleSpots[i][p].s / 2) + scrollOffset.y, particleSpots[i][p].s, particleSpots[i][p].s);    
+        } 
+    }
     
     //enemies
     for (i = 0; i < enemies.length; i++) {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = enemies[i].color;
         
         //draw enemies on screen
         if (!(enemies[i].x  + scrollOffset.x + 960 < 0 || enemies[i].x  + scrollOffset.x + 960 > c.width ||
             enemies[i].y  + scrollOffset.y + 540 < 0 || enemies[i].y  + scrollOffset.y + 540 > c.height)) {
             ctx.fillRect(enemies[i].x - (enemies[i].s / 2) + scrollOffset.x, enemies[i].y - (enemies[i].s / 2) + scrollOffset.y, enemies[i].s, enemies[i].s);
+            
+            //draw healthbars
+            
+            //bar empty
+            ctx.fillStyle = "rgba(200,200,200,0.5)";
+            ctx.fillRect(enemies[i].x - (enemies[i].s / 2) + scrollOffset.x - 10, enemies[i].y - (enemies[i].s / 2) + scrollOffset.y - 15, 
+                         50, 10)
+            
+            //bar color fill
+            ctx.fillStyle = "red";
+            ctx.fillRect(enemies[i].x - (enemies[i].s / 2) + scrollOffset.x - 10, enemies[i].y - (enemies[i].s / 2) + scrollOffset.y - 15, 
+                         ((enemies[i].h / 1000) * 50), 10);
+            
+            //bar outline
+            ctx.strokeStyle = "rgba(0,0,0,0.8)";
+            ctx.lineWidth = "2";
+            ctx.strokeRect(enemies[i].x - (enemies[i].s / 2) + scrollOffset.x - 10, enemies[i].y - (enemies[i].s / 2) + scrollOffset.y - 15, 
+                         50, 10)
         }
     }
     
@@ -194,23 +250,40 @@ function draw() {
     ctx.fillRect(-25,-25,50,50)
     
     for (i = 0; i < enemies.length; i++) {
-        ctx.fillStyle = "red";
+        ctx.fillStyle = enemies[i].color;
         
         //draw enemies on screen
         if (!(enemies[i].x  + scrollOffset.x + 960 < 0 - 240 || enemies[i].x  + scrollOffset.x + 960 > c.width + 240 ||
             enemies[i].y  + scrollOffset.y + 540 < 0 - 135 || enemies[i].y  + scrollOffset.y + 540 > c.height + 135)) {
             ctx.fillRect(enemies[i].x - (enemies[i].s / 2) + scrollOffset.x, enemies[i].y - (enemies[i].s / 2) + scrollOffset.y, enemies[i].s, enemies[i].s);
+            
         }
     }
     ctx.restore();
     
     //health Bar
-    ctx.fillStyle = "lime";
+    //bar background
+    ctx.fillStyle = "rgba(200,200,200,0.5)";
+    ctx.fillRect(460 ,c.height / 2 - 15 + 500 ,1000,30)
+    
+    //bar green
+    ctx.fillStyle = barColor;
     ctx.fillRect(460 ,c.height / 2 - 15 + 500 ,player.health * 10,30)
     
+    //bar outline
+    ctx.strokeStyle = "rgba(0,0,0,0.8)";
+    ctx.lineWidth = "4";
+    ctx.strokeRect(460 ,c.height / 2 - 15 + 500 ,1000,30)
+    
+    //score text
     ctx.fillStyle = "black";
     ctx.font = "40px roboto";
     ctx.fillText("Score: " + score, c.width - 200,60);
+    
+    //location
+    ctx.fillStyle = "black";
+    ctx.font = "40px roboto";
+    ctx.fillText("X: " + scrollOffset.x + " Y: " + scrollOffset.y, 20, 320);
 }
 
 var f = 90 / 180 * Math.PI;
@@ -278,6 +351,57 @@ function playerMove() {
                 
     }
     
+    
+    if (player.health <= 0) {
+        EnemyMoveSpeed = 0;
+        player.health = 0;
+        gameOver = true;
+    }
+    
+    if (!player.canDamage) {
+        player.gracePeriodCount += 1;
+        
+        if (player.gracePeriodCount >= 0 && player.gracePeriodCount < 17.5) {
+            barColor = "red"
+            player.image = playerDamageImg;
+        }else if (player.gracePeriodCount >= 17.5 && player.gracePeriodCount < 35) {
+            barColor = "lime"
+            player.image = playerIMG;
+        }else if (player.gracePeriodCount >= 35 && player.gracePeriodCount < 52.5) {
+            barColor = "red"
+            player.image = playerDamageImg;
+        }else if (player.gracePeriodCount >= 52.5 && player.gracePeriodCount < 70) {
+            barColor = "lime"
+            player.image = playerIMG;
+        }
+    }
+    
+    if (!player.canDamage && player.gracePeriodCount > 70) {
+        player.gracePeriodCount = 0;
+        player.canDamage = true;
+        barColor = "lime";
+    }
+    
+    
+    if (score > 100 && s1) {
+        for (i = 0; i< 2000; i++) {
+            SpawnEnemy(300, 100, "red", 10);
+            
+        }
+        s1 = false
+    }
+    if (score > 500 && s2) {
+        
+        for (i = 0; i< 1000; i++) {
+            SpawnEnemy(400, 500, "green", 20);
+        }
+        s2 = false
+    }
+    
+}
+
+
+function EnemyMove() {
     //enemies follow temp
     for (i = 0; i < enemies.length; i++) {
         if (enemies[i].x + scrollOffset.x > player.x - enemies[i].r && enemies[i].x + scrollOffset.x < player.x + enemies[i].r &&
@@ -301,38 +425,74 @@ function playerMove() {
             if (enemies[i].y + scrollOffset.y > player.y) {
                 enemies[i].y -= EnemyMoveSpeed;
             }
+            
+            //ANTI ENEMY CLUMP
+            for (e = 0; e < enemies.length; e++) {
+                if (enemies[i].x + scrollOffset.x - enemies[i].s < enemies[e].x + enemies[e].s + scrollOffset.x && 
+                    enemies[i].x + scrollOffset.x + enemies[i].s > enemies[e].x - enemies[e].s + scrollOffset.x &&
+                    enemies[i].y + scrollOffset.y - enemies[i].s < enemies[e].y + enemies[e].s + scrollOffset.y &&
+                    enemies[i].y + scrollOffset.y + enemies[i].s > enemies[e].y - enemies[e].s + scrollOffset.y) {
+                    //right
+                    if (enemies[i].x + scrollOffset.x < enemies[e].x + scrollOffset.x) {
+                        enemies[i].x -= 1;
+                    }
+
+                    //left
+                    if (enemies[i].x + scrollOffset.x > enemies[e].x + scrollOffset.x) {
+                        enemies[i].x += 1;
+                    }
+
+                    //down
+                    if (enemies[i].y + scrollOffset.y < enemies[e].y + scrollOffset.y) {
+                        enemies[i].y -= 1;
+                    }
+
+                    //up
+                    if (enemies[i].y + scrollOffset.y > enemies[e].y + scrollOffset.y) {
+                        enemies[i].y += 1;
+                    }
+                }
+                
+            }
+            
         }
         
+        //MOVE TO PLAYER SLOWLY
         //right
-            if (enemies[i].x + scrollOffset.x < player.x) {
-                enemies[i].x += 0.2;
-            }
-            
-            //left
-            if (enemies[i].x + scrollOffset.x > player.x) {
-                enemies[i].x -= 0.2;
-            }
-            
-            //down
-            if (enemies[i].y + scrollOffset.y < player.y) {
-                enemies[i].y += 0.2;
-            }
-            
-            //up
-            if (enemies[i].y + scrollOffset.y > player.y) {
-                enemies[i].y -= 0.2;
-            }
+        if (enemies[i].x + scrollOffset.x < player.x) {
+            enemies[i].x += 0.2;
+        }
+
+        //left
+        if (enemies[i].x + scrollOffset.x > player.x) {
+            enemies[i].x -= 0.2;
+        }
+
+        //down
+        if (enemies[i].y + scrollOffset.y < player.y) {
+            enemies[i].y += 0.2;
+        }
+
+        //up
+        if (enemies[i].y + scrollOffset.y > player.y) {
+            enemies[i].y -= 0.2;
+        }
         
+        //ENEMY COLLISION WITH PLAYER
         if (enemies[i].x + scrollOffset.x > player.x - 25 && enemies[i].x + scrollOffset.x < player.x + 25 &&
            enemies[i].y + scrollOffset.y < player.y + 25 && enemies[i].y + scrollOffset.y > player.y - 25) {
-            player.health--;
+            DamagePlayer(enemies[i].damage);
+            
         }
     }
-    
-    if (player.health <= 0) {
-        EnemyMoveSpeed = 0;
-        player.health = 0;
-        gameOver = true;
+}
+
+
+function DamagePlayer(amount) {
+    if (player.canDamage) {
+        player.health -= amount;
+        barColor = "red";
+        player.canDamage = false;
     }
 }
 
@@ -342,13 +502,12 @@ function bulletMove() {
         player.bullets[i].x += player.bullets[i].xv;
         player.bullets[i].y += player.bullets[i].yv;
      
+        //bullet enemy collision
         for (e = 0; e < enemies.length; e++) {
             if (player.bullets[i].x < enemies[e].x + enemies[e].s && player.bullets[i].x > enemies[e].x - enemies[e].s &&
                player.bullets[i].y < enemies[e].y + enemies[e].s && player.bullets[i].y > enemies[e].y - enemies[e].s) {
-                player.bullets.splice(i,1);
-                enemies.splice(e,1);
-                score++;
-                SpawnEnemy(300);
+                KillEnemy(i, e, enemies[e].x, enemies[e].y)
+                SpawnEnemy(300, 100, "red", 10);
                 break;
             }
             
@@ -372,6 +531,27 @@ function bulletMove() {
     }
 }
 
+function KillEnemy(bullet, enemy, ex, ey) {
+    player.bullets.splice(bullet,1);
+    
+    enemies[enemy].h -= player.gun.d;
+    if (enemies[enemy].h <= 0) {
+        var parts = [
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+            new particle(ex, ey, 5, "red", 50),
+        ]
+        particleSpots.push(parts);
+        enemies.splice(e,1);
+        score++;    
+    }
+}
+
 function Shoot() {
     player.image = playerShootIMG;
     player.bullets.push(new bull(-scrollOffset.x, -scrollOffset.y, player.gun.r, player.gun.s));
@@ -381,11 +561,56 @@ function update(time = 0) {
     dt = time - lastTime;
     lastTime = time;
     if (!gameOver) {
+        particlesAnim();
+        EnemyMove();
         playerMove();
         bulletMove();
     }
     draw();
     requestAnimationFrame(update);
+}
+
+function particlesAnim() {
+    for (i = 0; i < particleSpots.length; i++) {
+        for (p = 0; p < particleSpots[i].length; p++) {
+            particleSpots[i][p].count++;
+            
+            if (p == 0) {
+                particleSpots[i][p].y -= 1.5;
+            }
+            if (p == 1) {
+                particleSpots[i][p].x += 1;
+                particleSpots[i][p].y -= 1;
+            }
+            if (p == 2) {
+                particleSpots[i][p].x += 1.5;
+            }
+            if (p == 3) {
+                particleSpots[i][p].x += 1;
+                particleSpots[i][p].y += 1;
+            }
+            if (p == 4) {
+                particleSpots[i][p].y += 1.5;
+            }
+            if (p == 5) {
+                particleSpots[i][p].x -= 1;
+                particleSpots[i][p].y += 1;
+            }
+            if (p == 6) {
+                particleSpots[i][p].x -= 1.5;
+            }
+            if (p == 7) {
+                particleSpots[i][p].x -= 1;
+                particleSpots[i][p].y -= 1;
+            }
+            
+        }
+        
+        if (particleSpots[i][0].count > particleSpots[i][0].decay) {
+            particleSpots.splice(i, 1);
+        }
+    }
+
 }
 
 function find_angle(A,B,C) {
@@ -455,6 +680,14 @@ document.addEventListener("keydown", event => {
         //SPACE
         player.shoot = true;
         player.image = playerShootIMG;
+    }else if (event.keyCode == 49) {
+        player.gun = guns.pistol;
+    }else if (event.keyCode == 50) {
+        player.gun = guns.ak;
+    }else if (event.keyCode == 51) {
+        player.gun = guns.pistol;
+    }else if (event.keyCode == 52) {
+        player.gun = guns.pistol;
     }
    
     
