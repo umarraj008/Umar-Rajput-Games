5b01f11db01af66269c058cc87a14b8025fdf60e
Umar Rajput <47900850+umarraj008@users.noreply.github.com>
2019-10-17 21:34:53 +0100

    Add files via upload

--- ParticleSystems/Starfield/starfield.js
+++ ParticleSystems/Starfield/starfield.js
@@ -1,6 +1,11 @@
-
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
+
+addEventListener("resize", event => {
+    c.width = window.innerWidth;
+    c.height = window.innerHeight;
+
+});
 
 class star {
     constructor() {
@@ -8,14 +13,17 @@ class star {
         this.y = genRand(-c.height / 2, c.height / 2, 1);
         this.z = genRand(0, 800, 1);
         this.size = genRand(1, 7, 1);
+        this.col1 =genRand(0, 255, 1);
+        this.col2 =genRand(0, 255, 1);
+        this.col3 =genRand(0, 255, 1);
+        this.bloom = genRand(5, 32, 1);
     }
 }
 
 var stars = [];
-var starsAmount = 200;
+var starsAmount = 400;
 var dt = 0;
 var lastTime = 0;
-
 for (i = 0; i < starsAmount; i++) {
     stars.push(new star())
 }
@@ -30,11 +38,13 @@ function update(time = 0) {
     ctx.save()
     ctx.translate(c.width / 2, c.height /2 );
 
+
     for (i = 0; i < stars.length; i++) {
         var s = stars[i];
         var sx = map(s.x / s.z, 0, 1, 1, c.width / 2);
         var sy = map(s.y / s.z, 0, 1, 1, c.height / 2);
         var r = Math.abs(map(s.z, 0, c.width, s.size, 0));
+        var opacity = Math.abs(map(s.z, 0, c.width, s.bloom, 0))
 
         s.z -= 0.2 * dt;
 
@@ -42,11 +52,24 @@ function update(time = 0) {
             s.x = genRand(-c.width / 2, c.width / 2, 1);
             s.y = genRand(-c.height / 2, c.height / 2, 1);
             s.z = 800;
+            s.col1 =genRand(0, 255, 1);
+            s.col2 =genRand(0, 255, 1);
+            s.col3 =genRand(0, 255, 1);
+            s.bloom = genRand(5, 32, 1);
         }
 
-        ctx.fillStyle = "white";
         ctx.beginPath();
+        ctx.fillStyle = "rgba("+s.col1+","+s.col2+","+s.col3+",1"
         ctx.ellipse(sx, sy, r, r, Math.PI, Math.PI * 2, 0, 0);
+        ctx.fill();
+
+        var radialGradient = ctx.createRadialGradient(sx, sy, r / r, sx, sy, r * opacity);
+        radialGradient.addColorStop(0, "rgba("+s.col1+","+s.col2+","+s.col3+",0.2)");
+        radialGradient.addColorStop(1, "rgba(0,0,0,0)");
+
+        ctx.beginPath();
+        ctx.fillStyle = radialGradient;
+        ctx.ellipse(sx, sy, r * s.bloom, r * s.bloom, Math.PI, Math.PI * 2, 0, 0);
         ctx.fill();
 
     }
